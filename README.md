fae7d1cd651dc3a401a2851867975f87edb5aaed
Mostafa Ahmadi <s.mostafa.a96@gmail.com>
2020-07-28 16:52:52 +0430

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Luna16
-over view on the works done at detection track of luna16 challenge
+# Luna16 object detection
+Trying to develop a readable documented repository for Lung Nodule Detection. (Based on the approach of team "[grt123](https://github.com/lfz/DSB2017)" which stood on first place of DSB2017)
+
+Basically I am going through their paper, hence their code (except for their model) was not readable to me at all.
+
+I hope it helps researchers.
+If you have any question on the code, please send an [email to me](mailto:s.mostafa.a96@gmail.com?subject=[GitHub]%20LUNA16%20grt123).
--- preprocess/utility.py
+++ preprocess/utility.py
@@ -1,8 +1,14 @@
 from functools import partial
 
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy import ndimage as ndi
 from scipy.ndimage.interpolation import zoom
 import scipy
+from skimage.filters import roberts
+from skimage.measure import label, regionprops
+from skimage.morphology import convex_hull_image, disk, binary_closing
+from skimage.segmentation import clear_border
 
 
 def _get_cube_from_img_new(img, origin: tuple, block_size=128, pad_value=106):
@@ -185,6 +191,147 @@ def get_augmented_cube(img: np.array, radius: float, origin: tuple, spacing: tup
 # print(new_img[new_out])
 # new_img = get_cube_from_img_new(tst_img, origin=tuple((130, 130, 130)), block_size=100, pad_value=1)
 # print(new_img)
+
+
+def get_segmented_lungs(im, plot=False):
+    '''
+    This funtion segments the lungs from the given 2D slice.
+    image label: 0
+    '''
+    plt_number = 0
+    if plot:
+        f, plots = plt.subplots(12, 1, figsize=(5, 40))
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(im, cmap=plt.cm.bone)
+        plt_number += 1
+
+    '''
+    Step 1: Convert into a binary image. 
+    image label: 1
+    '''
+    binary = im < -604
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(binary, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 2: Remove the blobs connected to the border of the image.
+    image label: 2
+    '''
+    cleared = clear_border(binary)
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(cleared, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 3: Label the image.
+    image label: 3
+    '''
+    label_image = label(cleared)
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(label_image, cmap=plt.cm.bone)
+        plt_number += 1
+
+    '''
+    Step 4: Keep the labels with 2 largest areas and segment two lungs.
+    image label: 4
+    '''
+    areas = [r.area for r in regionprops(label_image)]
+    areas.sort()
+    labels = []
+    if len(areas) > 2:
+        for region in regionprops(label_image):
+            if region.area < areas[-2]:
+                for coordinates in region.coords:
+                    label_image[coordinates[0], coordinates[1]] = 0
+            else:
+                coordinates = region.coords[0]
+                labels.append(label_image[coordinates[0], coordinates[1]])
+    else:
+        labels = [1, 2]
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(label_image, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 5: Fill in the small holes inside the mask of lungs which we seperate right and left lung. r and l are symbolic and they can be actually left and right!
+    image labels: 5, 6
+    '''
+    r = label_image == labels[0]
+    l = label_image == labels[1]
+    r_edges = roberts(r)
+    l_edges = roberts(l)
+    r = ndi.binary_fill_holes(r_edges)
+    l = ndi.binary_fill_holes(l_edges)
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(r, cmap=plt.cm.bone)
+        plt_number += 1
+
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(l, cmap=plt.cm.bone)
+        plt_number += 1
+
+    '''
+    Step 6: convex hull of each lung
+    image labels: 7, 8
+    '''
+    r = convex_hull_image(r)
+    l = convex_hull_image(l)
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(r, cmap=plt.cm.bone)
+        plt_number += 1
+
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(l, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 7: joint two separated right and left lungs.
+    image label: 9
+    '''
+    sum_of_lr = r + l
+    binary = sum_of_lr > 0
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(binary, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 8: Closure operation with a disk of radius 10. This operation is 
+    to keep nodules attached to the lung wall.
+    image label: 10
+    '''
+    selem = disk(10)
+    binary = binary_closing(binary, selem)
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(binary, cmap=plt.cm.bone)
+        plt_number += 1
+    '''
+    Step 9: Superimpose the binary mask on the input image.
+    image label: 11
+    '''
+    get_high_vals = binary == 0
+    im[get_high_vals] = 0
+    if plot:
+        plots[plt_number].axis('off')
+        plots[plt_number].set_title(f'{plt_number}')
+        plots[plt_number].imshow(im, cmap=plt.cm.bone)
+        plt_number += 1
+
+    return im
 
 
 tst_img = np.ones((280, 300, 270), dtype=float)
